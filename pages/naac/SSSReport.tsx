f38af662955e53d555fdaa2ae67dd0ac465594d9
Sumrita Sawant <sumritasawant10.1@gmail.com>
2026-03-15 13:07:23 +0530

    Revert "Update SSS report header"
    This reverts commit bc0797de70ad52889b745d7edd900f0c129de315.

--- pages/naac/SSSReport.tsx
+++ pages/naac/SSSReport.tsx
@@ -27,7 +27,12 @@ const sssReportLinks = [
 const SSSReport: React.FC = () => {
   return (
     <div className="sssr-page">
-      <SSRHeader />
+      <header className="sssr-hero">
+        <div className="sssr-hero-content">
+          <h1 className="sssr-title">SSS Report</h1>
+          <div className="sssr-breadcrumb">Home &raquo; SSS Report</div>
+        </div>
+      </header>
 
       <main className="sssr-content">
         <div className="sssr-grid">
